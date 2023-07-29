7d694d4e40836ae627d7e9088733846fbf4cc420
Renggars <renggarendy22@gmail.com>
2023-07-29 09:16:34 +0700

    upload soal ke-dua

---
+++ Part5.5-Soal3.js
@@ -0,0 +1,12 @@
+let word = 'wow JavaScript is so cool';
+let exampleFirstWord3 = word.substring(0,3);
+let exampleSecondWord3 = word.substring(4,14);
+let exampleThirdWord3 = word.substring(15,17);
+let exampleFourthWord3 = word.substring(18,20);
+let exampleFifethWord3 = word.substring(21,25);
+
+console.log('first word : ' + exampleFirstWord3);
+console.log('Second word : ' + exampleSecondWord3);
+console.log('Third word : ' + exampleThirdWord3);
+console.log('Fourth word : ' + exampleFourthWord3);
+console.log('Fifeth word : ' + exampleFifethWord3);
---
+++ Part5.5-Soal4.js
@@ -0,0 +1,18 @@
+let word4 = 'wow JavaScript is so cool';
+let exampleFirstWord4 = word4.substring(0,3);
+let exampleSecondWord4 = word4.substring(4,14);
+let exampleThirdWord4 = word4.substring(15,17);
+let exampleFourthWord4 = word4.substring(18,20);
+let exampleFifethWord4 = word4.substring(21,25);
+
+let firstWordLenght = exampleFifethWord4.length;
+let secondWordLenght = exampleSecondWord4.length;
+let thirdWordLenght = exampleThirdWord4.length;
+let fourthWordLenght = exampleFourthWord4.length;
+let fifethWordLenght = exampleFifethWord4.length;
+
+console.log('first word : ' + exampleFirstWord4 + ', with length : ' + firstWordLenght);
+console.log('Second word : ' + exampleSecondWord4  + ', with length : ' + secondWordLenght);
+console.log('Third word : ' + exampleThirdWord4 + ', with length : ' + thirdWordLenght);
+console.log('Fourth word : ' + exampleFourthWord4 + ', with length : ' + fourthWordLenght);
+console.log('Fifeth word : ' + exampleFifethWord4 + ', with length : ' + fifethWordLenght);
